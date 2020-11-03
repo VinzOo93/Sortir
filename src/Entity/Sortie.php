3311bd89e28799e3f01f78ad178a994486e965cd
VinzOo93 <v.orru@live.fr>
2020-11-03 17:42:41 +0100

    VO: Modification entité Sortie Duree en Integer

--- src/Entity/Sortie.php
+++ src/Entity/Sortie.php
@@ -28,7 +28,7 @@ class Sortie
     private $dateHeureDebut;
 
     /**
-     * @ORM\Column(type="datetime", length=255)
+     * @ORM\Column(type="integer")
      */
     private $duree;
 
@@ -81,12 +81,12 @@ class Sortie
         return $this;
     }
 
-    public function getDuree(): \DateTimeInterface
+    public function getDuree(): ?int
     {
         return $this->duree;
     }
 
-    public function setDuree(\DateTimeInterface $duree): self
+    public function setDuree(int $duree): self
     {
         $this->duree = $duree;
 
