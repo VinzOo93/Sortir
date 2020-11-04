9e13e8582232a840ebc522c601925acd395c2033
VinzOo93 <v.orru@live.fr>
2020-11-04 12:09:27 +0100

    VO: Fixture devrait debugger fonctionnel sur l'entité sortie à tester pour l'entité USER

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -10,10 +10,8 @@ use Doctrine\Persistence\ObjectManager;
 class AppFixtures extends Fixture
 {
     const NOM = ['aller à la place', 'aller faire du cheval', 'faire du snow bord', 'boire de bières', 'ceuillir des cerises', 'manger des abricots'];
-    const DHD = ['03/10/2020 04:15:50', '26/09/2020 15:04:03', '08/10/2020 21:39:10', '12/10/2020 11:37:17', '14/09/2020 06:29:19', '31/10/2020 23:40:33'];
     const DUREE = [90,60,30,240,70,150];
     const NBMAX = [5,54,12,50,30,15];
-    const DLIM = ['03/11/2020 00:00:00', '03/11/2020 00:00:00', '03/11/2020 00:00:00', '03/11/2020 00:00:00', '03/11/2020 00:00:00', '03/11/2020 00:00:00'];
     const INFOS = ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eleifend, libero in ullamcorper blandit, eros magna commodo ligula, eu fermentum eros urna posuere leo. Mauris semper purus et purus faucibus.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eleifend, libero in ullamcorper blandit, eros magna commodo ligula, eu fermentum eros urna posuere leo. Mauris semper purus et purus faucibus.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eleifend, libero in ullamcorper blandit, eros magna commodo ligula, eu fermentum eros urna posuere leo. Mauris semper purus et purus faucibus.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eleifend, libero in ullamcorper blandit, eros magna commodo ligula, eu fermentum eros urna posuere leo. Mauris semper purus et purus faucibus.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eleifend, libero in ullamcorper blandit, eros magna commodo ligula, eu fermentum eros urna posuere leo. Mauris semper purus et purus faucibus.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eleifend, libero in ullamcorper blandit, eros magna commodo ligula, eu fermentum eros urna posuere leo. Mauris semper purus et purus faucibus.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eleifend, libero in ullamcorper blandit, eros magna commodo ligula, eu fermentum eros urna posuere leo. Mauris semper purus et purus faucibus.'];
     const ETAT = [4,1,5,3,2,2];
 
@@ -22,13 +20,13 @@ class AppFixtures extends Fixture
         for ($i = 0; $i < 5; $i++) {
             $sortie = new  Sortie();
             $sortie->setNom(self::NOM[$i]);
-            $sortie->setDateHeureDebut(self::DHD[$i]);
+            $sortie->setDateHeureDebut(new \DateTime('now'), date_interval_create_from_date_string('+ 61 days'));
             $sortie->setDuree(self::DUREE[$i]);
             $sortie->setNbInscriptionMax(self::NBMAX[$i]);
-            $sortie->setDateLimiteInscription(self::DLIM[$i]);
-            $sortie->setInfosSortie(INFO[$i]);
+            $sortie->setDateLimiteInscription(new \DateTime('now'), date_interval_create_from_date_string('+ 90 days'));
+            $sortie->setInfosSortie(self::INFOS[$i]);
             $sortie->setEtat(self::ETAT[$i]);
-            $manager->persist($sortie[$i]);
+            $manager->persist($sortie);
         }
         // $product = new Product();
         // $manager->persist($product);
