250a4858ee1baed78ea14d239d125280631f70ae
VinzOo93 <v.orru@live.fr>
2020-11-06 17:41:03 +0100

    VO:
    modification constructeur en Array collection affichage fonctionne
    debug fixture en cours

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -62,7 +63,7 @@ class User implements UserInterface
 
     public function __construct()
     {
-        $this->user =new User;
+        $this->user =new ArrayCollection();
     }
 
     /**
