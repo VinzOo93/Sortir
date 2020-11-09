ebc659c311cfc75508d7390a335d6931aa08e0f3
VinzOo93 <v.orru@live.fr>
2020-11-09 17:10:01 +0100

    VO:
    Creation du filter des sorties dans le template SortieAcceuil.html.twig

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -15,10 +15,21 @@ class SortieController extends AbstractController
     public function home(): Response
     {
         $sortieRepo = $this->getDoctrine()->getRepository(Sortie::class);
-        $sorties = $sortieRepo -> findAll();
+        $sorties = $sortieRepo ->  findAll();
 
         return $this->render('sortie/SortieAcceuil.html.twig', [
             "sorties" => $sorties
         ]);
     }
+
+    public function filter($sortieRepository, $form, $inscrit, $data) {
+
+        $sortieRepo = $sortieRepository->filter($data, $inscrit);
+        return $this->render('sortie/SortieAcceuil.html.twig', [
+            'sorties' => $sortieRepo,
+            'inscrit' =>$inscrit,
+            'form' => $form->createView(),
+        ]);
+    }
+
 }
--- src/Repository/SortieRepository.php
+++ src/Repository/SortieRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Sortie;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -19,22 +20,52 @@ class SortieRepository extends ServiceEntityRepository
         parent::__construct($registry, Sortie::class);
     }
 
-    // /**
-    //  * @return Sortie[] Returns an array of Sortie objects
-    //  */
-    /*
-    public function findByExampleField($value)
+
+    public function filter($data, $inscrit)
     {
-        return $this->createQueryBuilder('s')
-            ->andWhere('s.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('s.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
+        $query = $this
+            ->createQueryBuilder('s')
+            ->select('o', 's', 'c', 'p')
+            ->leftJoin('s.organisateur', 'o')
+            ->leftJoin('s.siteOrganisateur', 'c')
+            ->leftJoin('s.inscrit', 'p')
+            ->andWhere("DATE_ADD(DATE_ADD(s.dateHeureDebut, s.duree, 'minute'),1, 'month' -> CURRENT_TIMESTAMP()");;
+        if (!empty($search->campus)) {
+            $query = $query
+                ->andWhere('c.id = :val')
+                ->setParameter('val', $search->campus);
+
+        }
+        if (!empty($search->q)) {
+            $query = $query
+                ->andWhere('s.nom LIKE :q')
+                ->setParameter('q', "%{$search->q}");
+        }
+        if (!empty($search->d)) {
+            $query = $query
+                ->andWhere('s.dateHeureDebut BETWEEN :from AND :to')
+                ->setParameter('from', $search->dateHeureDebut)
+                ->setParameter('to', $search->to);
+        }
+        if (!empty($search->o)) {
+            $query = $query
+                ->select('o LIKE app.user.id');
+        }
+        if (!empty($search->p)) {
+            $query = $query
+                ->select('p LIKE app.user.id');
+        }
+        if (!empty($search)) {
+            $query = $query
+                ->select('p NOT LIKE app.user.id');
+        }
+        if (!empty($search->s)) {
+            $query = $query
+                ->select('s.dateHeureDebut < NOW');
+        }
     }
-    */
+
+}
 
     /*
     public function findOneBySomeField($value): ?Sortie
@@ -47,4 +78,4 @@ class SortieRepository extends ServiceEntityRepository
         ;
     }
     */
-}
+
