83c7e4d598114623b95146f3b989a60d6f3d64de
Antoine202062 <antoine.dewez62@gmail.com>
2020-11-13 14:37:05 +0100

    AD: modification twig et cs update

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -116,7 +116,6 @@ class RegistrationController extends AbstractController
 
             $entityManager->persist($user);
             $entityManager->flush();
-
             $this->addFlash('success','Utilisateur modifié avec succès');
 
             return $this->redirectToRoute('sortie');
